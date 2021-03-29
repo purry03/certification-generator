3830652ae42c7212929c43fbd6a92b4c5cd3fe55
purry03 <rohansh.lko@gmail.com>
2021-03-29 22:12:35 +0530

    Rebuild

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const loaders = require("./loaders");
-const modelLoader = require("./models");
-const controllers = require("./controllers");
+const app = express();
 
-async function startServer() {
-  const app = express();
-  const modules = await loaders.init(app);
-  await modelLoader(modules.mongooseDB);
-  await controllers.init(app);
-  app.listen(process.env.PORT || 80, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(`Server Online`);
-  });
-}
+app.listen(process.env.PORT || 80, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Server Online`);
+});
 
-startServer();
-global.__basedir = __dirname;
+app.get("/", function (res, res) {
+  res.setEncoding("hello");
+});
